46201f19b0cdd1bce2af92bff0e5275e075ca943
omar <eng.omar.amr@gmail.com>
2023-03-14 17:07:35 +0200

    mod

--- Registers_and_Counters/register_file_TB.v
+++ Registers_and_Counters/register_file_TB.v
@@ -21,7 +21,7 @@
 
 
 module register_file_TB;
-        parameter W = 5, B = 8;
+        parameter W = 5, B = 32;
         reg [W-1 : 0] r_addr_A, r_addr_B, w_addr;
         reg clk, wr_en, n_reset;
         reg [B-1 : 0] w_data;
@@ -40,7 +40,9 @@ module register_file_TB;
         );
 
         always #15 clk = ~clk;
-        initial begin
+
+        task init_reg_file();
+        begin
             clk = 0;
             r_addr_A =0;
             r_addr_B = 0;
@@ -48,18 +50,50 @@ module register_file_TB;
             wr_en = 0;
             n_reset = 0;
             w_data  = 0;
-            #20;
+            @(negedge clk);
             n_reset = 1;
-            wr_en = 1;
-            w_addr = 5'd0;
-            w_data = 8'd10;
-            #30;
-            w_addr = 5'd1;
-            w_data = 8'd15;
-            #30;
-            r_addr_A = 5'd0;
-            r_addr_B = 5'd1;
-            #30;
+        end
+        endtask
+
+        task reg_write(
+            input [W-1: 0] address,
+            input [B-1: 0] data
+        );
+            begin
+                wr_en = 1;
+                w_addr = address;
+                w_data = data;
+                @(negedge clk);
+                wr_en = 0;
+                w_addr = 0;
+                w_data = 0;
+            end
+        endtask
+
+        task reg_read(
+            input [W-1: 0] addr1, addr2
+        );
+        begin
+            r_addr_A = addr1;
+            r_addr_B = addr2;
+        end
+        endtask
+
+        initial begin
+
+            init_reg_file();
+            reg_write(0,10);
+            reg_write(1,15);
+            
+            reg_read(0,1);
+            repeat(2) @(negedge clk);
+            
+            reg_write(20,100);
+            reg_write(21,200);
+            
+            reg_read(20,21);
+            repeat(2) @(negedge clk);
+
             $stop;
         end
 endmodule
